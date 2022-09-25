34fd7aaf33e6b11825b5113d590e62f48ebdb65c
maoforero <maforeror@gmail.com>
2022-09-25 02:08:29 -0500

    entrega #1

---
+++ src/com/example/Main.java
@@ -0,0 +1,27 @@
+package com.example;
+
+public class Main {
+
+    /**
+     * Primera entrega del curso basico de Java - OpenBootCamp
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        int ejemploEnteroInt = 10;
+        System.out.println("Int: " + ejemploEnteroInt);
+
+        long ejemploEnteroLong = 100000L;
+        System.out.println("Long: " + ejemploEnteroLong);
+
+        double ejemploDouble = 3.1416;
+        System.out.println("Double: " + ejemploDouble);
+
+        boolean ejemploBoolean = true;
+        System.out.println("Boolean: " + ejemploBoolean);
+
+        String ejemploString = "OpenBootCamp";
+        System.out.println("String: "+ ejemploString);
+
+    }
+}
